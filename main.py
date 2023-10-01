a44d2f52b3c0547dcee03331620fa6778c35da5a
DONKER963 <94936290+DONKER963@users.noreply.github.com>
2023-09-30 18:48:59 -0600

    Matriz

--- main.py
+++ main.py
@@ -2,12 +2,48 @@ import re
 from collections import Counter
 import pandas as pd
 import os
+import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
-def diccionario(rep):#metodo
+def diccionario(repositorio): #metodo para crear los diccionarios
+    textos = []
+    nombres = []
+    for doc in repositorio:#recorremos los archivos del repositorio
+        print(doc.name)
+        nombres.append(doc.name)
+        with open(doc.path,'r',encoding="utf-8") as file:#abrimos documento x doc
+            texto = file.read()#leemos el contenido
+            textos.append(texto)
+        palabras = re.findall(r'\b\w+\b',texto.lower())#Convertimos a minuscula y separamos palabras
+        #hacemos stopword
+        stop_words = set(stopwords.words('spanish'))
+        palabras_stopwords = [word for word in palabras if word.lower() not in stop_words]
+        #aplicamos stemming
+        stemmer = PorterStemmer()
+        palabras_stemming = [stemmer.stem(word) for word in palabras_stopwords]
+        #contamos las frecuencias de:
+        #texto limpio // texto con stopwords // texto con stemming
+        frecuencias =+ Counter(palabras)#cuenta las frecuencias
+        frecuencias_stopwords =+ Counter(palabras_stopwords)#cuenta las frecuencias
+        frecuencias_stemming = Counter(palabras_stemming)#cuenta las frecuencias
+    #creamos la tabla de frecuencias para el texto limpio
+    tabla_frecuencias = pd.DataFrame(data=frecuencias.items(), columns=['Palabra', 'Frecuencia'])#lo lista en 2 colm
+    tabla_frecuencias = tabla_frecuencias.sort_values(by='Frecuencia', ascending=False)#ordena
+    tabla_frecuencias.to_csv("diccionarios/diccionario.txt",index=False,sep=" ",header=False)#guarda
+    #creamos la tabla de frecuencias para el texto sin stopwords
+    tabla_frecuencias_stopwords = pd.DataFrame(data=frecuencias_stopwords.items(), columns=['Palabra', 'Frecuencia'])#lo lista en 2 colm
+    tabla_frecuencias_stopwords = tabla_frecuencias_stopwords.sort_values(by='Frecuencia', ascending=False)#ordena
+    tabla_frecuencias_stopwords.to_csv("diccionarios/diccionario_stopwords.txt",index=False,sep=" ",header=False)#guarda
+    #creamos la tabla de frecuencias para el texto con stemming
+    tabla_frecuencias_stemming = pd.DataFrame(data=frecuencias_stemming.items(), columns=['Palabra', 'Frecuencia'])#lo lista en 2 colm
+    tabla_frecuencias_stemming = tabla_frecuencias_stemming.sort_values(by='Frecuencia', ascending=False)#ordena
+    tabla_frecuencias_stemming.to_csv("diccionarios/diccionario_stemming.txt",index=False,sep=" ",header=False)
+    return textos,nombres
+
+def diccionario_separados(rep):#metodo  diccionarios por separado
     for doc in rep:#recorremos todos los documentos en el repositorio
         print(doc.name)
         with open(doc.path, 'r',encoding="utf-8") as f:#abre documento
@@ -25,7 +61,7 @@ def diccionario(rep):#metodo
         frecuencias_stopwords = Counter(palabras_stopwords)#cuenta las frecuencias
         tabla_frecuencias_stopwords = pd.DataFrame(data=frecuencias_stopwords.items(), columns=['Palabra', 'Frecuencia'])#lo lista en 2 colm
         tabla_frecuencias_stopwords = tabla_frecuencias_stopwords.sort_values(by='Frecuencia', ascending=False)#ordena
-        tabla_frecuencias_stopwords.to_csv("diccionarios/diccionario_stopwords_"+doc.name,index=False,sep=":")
+        tabla_frecuencias_stopwords.to_csv("diccionarios/diccionario_stopwords_"+doc.name,index=False,sep=":",header=False)
         #Diccionario texto stemming
         #stem_words = SnowballStemmer('spanish')
         stemmer = PorterStemmer()
@@ -33,7 +69,7 @@ def diccionario(rep):#metodo
         frecuencias_stemming = Counter(palabras_stemming)#cuenta las frecuencias
         tabla_frecuencias_stemming = pd.DataFrame(data=frecuencias_stemming.items(), columns=['Palabra', 'Frecuencia'])#lo lista en 2 colm
         tabla_frecuencias_stemming = tabla_frecuencias_stemming.sort_values(by='Frecuencia', ascending=False)#ordena
-        tabla_frecuencias_stemming.to_csv("diccionarios_stemming/diccionario_stemming_"+doc.name,index=False,sep=":")
+        tabla_frecuencias_stemming.to_csv("diccionarios_stemming/diccionario_stemming_"+doc.name,index=False,sep=" ",header=False)
 
 def matrizBol(documentos,nombre_archivo):
      # Inicializar el vectorizador de conteo binario
@@ -63,13 +99,38 @@ def leer_documentos_stemming(cont):
                 nombre_archivos.append(nombre_archivo)
     return documentos,nombre_archivos
 
-############################   MAIN      ##############################
-contenido = 'repositorio'
-with os.scandir(contenido) as ficheros:
-    diccionario(ficheros)
+def crear_matriz_booleana(documentos, palabras, nombres):
+    matriz = np.zeros((len(documentos) + 1, len(palabras) + 1), dtype=object)
+    # Rellenar la primera fila con nombres de palabras
+    matriz[0, 1:] = palabras
+    # Rellenar la primera columna con nombres de documentos
+    matriz[1:, 0] = nombres
+    for i, doc in enumerate(documentos):
+        tokens = nltk.word_tokenize(doc)
+        for j, palabra in enumerate(palabras):
+            matriz[i + 1, j + 1] = int(palabra in tokens)
+    DF = pd.DataFrame(matriz)
+    DF.transpose
+    DF.to_csv("matriz de incidencias.csv",sep=",",header=False)
 
-documentos,nombre_archivos = leer_documentos_stemming(contenido)
+############################   MAIN      ##############################
+contenido = 'repositorio'#dado un repositorio
+documentos = []
+nombres = []
+palabras = []
+with os.scandir(contenido) as ficheros:
+    documentos, nombres = diccionario(ficheros)
+
+with open("diccionarios\diccionario_stemming.txt", 'r',encoding='utf-8') as archivo:
+    palabras = archivo.read().split()
+for i in palabras:
+    if i >='0' and i<='9':
+        palabras.remove(i)
+
+crear_matriz_booleana(documentos,palabras,nombres)
+
+#documentos,nombre_archivos = leer_documentos_stemming(contenido)
 # Crear la matriz binaria y guardarla en el archivo .txt
-matrizBol(documentos,nombre_archivos)
+#matrizBol(documentos,nombre_archivos)
 
     
